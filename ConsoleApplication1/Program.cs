74204e1ccfe5df1e0d705ce1c4ae5be825dc07de
Owner <Owner@DESKTOP>
2017-09-08 19:03:41 +0300

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char k; 
+            Console.WriteLine("Pls type a character.");
+            k = char.Parse(Console.ReadLine());
+            Console.WriteLine(" ");
+            Console.WriteLine("  " + k);
+            Console.Write(" " + k); Console.WriteLine(k);
+            Console.Write(k); Console.Write(k); Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
